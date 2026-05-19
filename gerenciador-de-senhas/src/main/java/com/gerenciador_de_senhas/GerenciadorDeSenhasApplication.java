efb6acc403a23e480a3dbc9da6cd031fe791691b
SandroNogueiraBittencourt <sandronogueirabittencourt@outlook.com>
2026-05-19 11:47:19 -0300

    Estrutura do projeto backend

---
+++ gerenciador-de-senhas/src/main/java/com/gerenciador_de_senhas/GerenciadorDeSenhasApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador_de_senhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeSenhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeSenhasApplication.class, args);
+	}
+
+}
