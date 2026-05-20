1c5c39e393c6134633b982e90caa614545ce54ab
SandroNogueiraBittencourt <sandronogueirabittencourt@outlook.com>
2026-05-20 20:35:37 -0300

    Adiciona repositories JPA

---
+++ gerenciador-de-senhas/src/main/java/com/gerenciador_de_senhas/repository/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package com.gerenciador_de_senhas.repository;
+
+import com.gerenciador_de_senhas.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    List<Categoria> findByUsuarioIdOrderByNomeAsc(Long usuarioId);
+
+    Optional<Categoria> findByIdAndUsuarioId(Long id, Long usuarioId);
+
+    boolean existsByNomeIgnoreCaseAndUsuarioId(String nome, Long usuarioId);
+}
---
+++ gerenciador-de-senhas/src/main/java/com/gerenciador_de_senhas/repository/SenhaRepository.java
@@ -0,0 +1,16 @@
+package com.gerenciador_de_senhas.repository;
+
+import com.gerenciador_de_senhas.entity.Senha;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SenhaRepository extends JpaRepository<Senha, Long> {
+
+    List<Senha> findByUsuarioIdOrderByNomeServicoAsc(Long usuarioId);
+
+    Optional<Senha> findByIdAndUsuarioId(Long id, Long usuarioId);
+
+    List<Senha> findByUsuarioIdAndNomeServicoContainingIgnoreCaseOrderByNomeServicoAsc(Long usuarioId, String nomeServico);
+}
